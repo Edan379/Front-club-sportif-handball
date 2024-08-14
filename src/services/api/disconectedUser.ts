1e4327273cf3ce1a52265fe7ffa009baccb2bb62
jordan <jordannigthwatch@hotmail.com>
2024-08-14 18:38:55 +0200

    typage du retour de la fonction disconectedUser()

--- src/services/api/disconectedUser.ts
+++ src/services/api/disconectedUser.ts
@@ -2,8 +2,7 @@ import { useApi } from "../hooks/useApi";
 
 const api = useApi();
 
-export async function disconectedUser() {
-  const response = await api.post("auth/logout");
-  console.log(response, response.status, response.data);
-  return response.status;
+export async function disconectedUser():Promise<number> {
+  const {status} = await api.post("auth/logout");
+  return status;
 }
